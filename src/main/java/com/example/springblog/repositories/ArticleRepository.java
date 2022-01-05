ed0aa946882f6518e5fa7d34923c33163a90cb11
svoby42 <qwerta009@gmail.com>
2022-01-05 11:49:39 +0100

    repositories

---
+++ src/main/java/com/example/springblog/repositories/ArticleRepository.java
@@ -0,0 +1,15 @@
+package com.example.springblog.repositories;
+
+import com.example.springblog.entities.Article;
+import com.example.springblog.entities.Role;
+import com.example.springblog.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+}
